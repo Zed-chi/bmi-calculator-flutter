0ad9400ce4a7f68a8760ac664cece436c9feae14
zed <zed@mail.ru>
2019-12-10 22:32:52 +0300

    modev consstants in file

---
+++ lib/constants.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColor = Color(0xff1d1e33);
+const kInactiveCardColor = Color(0xff111328);
+const kBottomColor = Color(0xffeb1555);
+const kLabelTextStyle = TextStyle(
+  fontSize: 18, 
+  color: Colors.white12
+);
+const kNumTextStyle = TextStyle(
+  fontSize:50,
+  fontWeight: FontWeight.w900
+);
