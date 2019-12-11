9f24d60a9e964417c9babb0fdcade5923e7491e1
alex <zed-chi@mail.ru>
2019-12-11 13:12:12 +0300

    added some style to buttons

--- lib/constants.dart
+++ lib/constants.dart
@@ -13,3 +13,24 @@ const kNumTextStyle = TextStyle(
   fontSize:50,
   fontWeight: FontWeight.w900
 );
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25,
+  fontWeight: FontWeight.bold
+);
+const kTitleTextStyle = TextStyle(
+  fontSize: 50,
+  fontWeight: FontWeight.bold
+);
+const kResultTextStyle = TextStyle(
+  color:Color(0xff24d76),
+  fontSize: 22,
+  fontWeight: FontWeight.bold
+);
+const kBMITextStyle = TextStyle(
+  fontWeight: FontWeight.bold,
+  fontSize: 100,  
+);
+const kBodyTextStyle = TextStyle(
+  fontSize:22,
+  
+);
